1c31691a2f8d9c17e580ae2c7aba84cd3b51fb7c
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2020-11-29 04:07:37 -0600

    Add files via upload

---
+++ Actividad_6/js/main.js
@@ -0,0 +1,59 @@
+var bLogeo = document.getElementById('signin');
+bLogeo.addEventListener('click', function () {
+    var e = document.getElementById('email').value;
+    var p = document.getElementById('password').value;
+    //const parametros = new URLSearchParams();
+ //   parametros.append('PrmEmail', e);
+   // parametros.append('PrmPassword', p);
+    // axios.get('http://localhost/hola?'+parametros)
+    //     .then(function (response) {
+    //         console.log(response)
+    //         console.log('contenido: ' + response.data)
+    //         console.log('estatus: ' + response.status)
+    //         document.getElementById('titulo').innerHTML = response.data
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error)
+    //     })
+
+
+    /*axios.post('http://localhost/adiosJson',{
+        PrmEmail: e,
+        PrmPassword: p
+    })
+        .then(function (response) {
+            console.log(response)
+            console.log('contenido: ' + response.data)
+            console.log('estatus: ' + response.status)
+            document.getElementById('titulo').innerHTML = response.data
+        })
+        .catch(function (error) {
+            console.log(error)
+    })*/
+    axios.post('http://127.0.0.1:4567/usuarios', {​​
+        PrtEmail : document.getElementById('email').value,
+        PrtPassword : document.getElementById('password').value
+    }​​)
+    .then(function (response) {​​
+        console.log(response)
+        console.log(response.data)
+        console.log(response.statusText)
+        document.getElementById('titulo').innerHTML = response.data
+    }​​)
+    .catch(function (error) {​​
+        console.log(error)
+    }​​)
+
+    
+
+    /*axios.post('http://localhost/adios?'+parametros)
+        .then(function (response) {
+            console.log(response)
+            console.log('contenido: ' + response.data)
+            console.log('estatus: ' + response.status)
+            document.getElementById('titulo').innerHTML = response.data
+        })
+        .catch(function (error) {
+            console.log(error)
+        })*/
+});
